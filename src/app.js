c6c3632f559f894337b34fe76140f4f9e448061f
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2024-09-15 16:04:14 +0200

    Devices

--- src/app.js
+++ src/app.js
@@ -1,24 +1,79 @@
-navigator.serial.addEventListener("connect", (e) => {
+const kc9531b = require("kc9531b");
+
+const {serial, SerialPort} = require("web-serial-polyfill");
+
+const navigatorSerial = navigator.serial || serial;
+
+const powerSpan = document.querySelector("#power");
+
+const openPort = port => {
+	port.open({
+		baudRate: 115200
+	}).then(() => {
+		const reader = port.readable.getReader();
+		const writer = port.writable.getWriter();
+
+		const callbacks = [];
+
+		const device = kc9531b({
+			on: (name, callback) => {
+				if(name === "data") {
+					callbacks.push(callback);
+				}
+			},
+
+			write: data => {
+				return writer.write(data);
+			}
+		});
+
+		const read = () => {
+			reader.read().then(data => {
+				callbacks.forEach(callback => callback(data.value));
+
+				if(data.done) {
+					setTimeout(() => {
+						read();
+					}, 100);
+				} else {
+					read();
+				}
+			});
+		};
+
+		read();
+
+		setInterval(() => {
+			device.getPowerdBm().then(power => {
+				powerSpan.innerHTML = `${power} dBm`;
+			});
+		}, 100);
+	});
+};
+
+navigatorSerial.addEventListener("connect", (e) => {
 	console.log(e);
 });
 
-navigator.serial.addEventListener("disconnect", (e) => {
+navigatorSerial.addEventListener("disconnect", (e) => {
 	console.log(e);
 });
 
-navigator.serial.getPorts().then((ports) => {
-	console.log(ports);
+navigatorSerial.getPorts().then((ports) => {
+	const port = ports[0];
+
+	if(port) {
+		openPort(port);
+	}
 });
 
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-	const usbVendorId = 0x2541;
-
-	navigator.serial
-		.requestPort({ filters: [{ usbVendorId }] })
+	navigatorSerial
+		.requestPort({ filters: [] })
 		.then((port) => {
-			console.log(port);
+			openPort(port);
 		})
 		.catch((e) => {
 			// The user didn't select a port.
